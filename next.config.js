eb2b31cebbc965e93df421080b23dc1a9c448d12
oliviertgpls-tech <oliviertgpls@gmail.com>
2025-08-31 22:55:11 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    // ajoute ici tous les domaines d’images que tu utilises
+    domains: ["images.unsplash.com"],
+    // ou utilise remotePatterns si tu veux être plus précis
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "images.unsplash.com",
+    //     pathname: "/**",
+    //   },
+    // ],
+  },
+};
+
 module.exports = nextConfig;
