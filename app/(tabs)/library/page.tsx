8f1223beea9a1757e224d5e00d7a62741ba69f50
oliviertgpls-tech <oliviertgpls@gmail.com>
2025-08-31 22:57:47 +0200

    Update page.tsx

--- app/(tabs)/library/page.tsx
+++ app/(tabs)/library/page.tsx
@@ -1,7 +1,7 @@
 export default function LibraryPage() {
     return (
       <section className="space-y-4">
-        <h1 className="text-2xl font-semibold">Mes recettes</h1>
+        <h1 className="text-2xl font-semibold">Ma Bibliothèque</h1>
         <p className="text-gray-600">Tes favoris, collections, etc.</p>
       </section>
     );
