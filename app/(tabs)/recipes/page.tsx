9d36decdf5783e1503b5ce302005954590097e16
oliviertgpls-tech <oliviertgpls@gmail.com>
2025-09-03 08:31:11 +0200

    Update page.tsx

--- app/(tabs)/recipes/page.tsx
+++ app/(tabs)/recipes/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useMemo, useState } from "react";
 import { useRecipes } from "../../providers";
 import { RecipeCard } from "../../../components/RecipeCard";
