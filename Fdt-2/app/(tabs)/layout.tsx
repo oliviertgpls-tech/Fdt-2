a79bdbca166a568a0664d276e316d796e0c568b2
oliviertgpls-tech <oliviertgpls@gmail.com>
2025-09-03 14:14:18 +0200

    Delete Fdt-2 directory

--- Fdt-2/app/(tabs)/layout.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-import { RecipesProvider } from "@/contexts/RecipesProvider";
-
-export default function TabsLayout({ children }: { children: React.ReactNode }) {
-  return <RecipesProvider>{children}</RecipesProvider>;
-}
