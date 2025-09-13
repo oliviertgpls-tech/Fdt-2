90e1ff3c868e73e5abf047dd60b8c794c7dddde8
oliviertgpls-tech <oliviertgpls@gmail.com>
2025-09-13 23:17:32 +0200

    Update layout.tsx

--- app/(tabs)/layout.tsx
+++ app/(tabs)/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { RecipesProvider } from "@/contexts/RecipesProvider";
+import { RecipesProvider } from "@/contexts/RecipesProviderWithDB";
 
 export default function TabsLayout({ children }: { children: React.ReactNode }) {
   return <RecipesProvider>{children}</RecipesProvider>;
