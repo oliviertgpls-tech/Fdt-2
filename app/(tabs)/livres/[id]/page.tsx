dbe45ee56ba779a461602a5f7efcd67cba68c864
oliviertgpls-tech <oliviertgpls@gmail.com>
2025-09-15 11:18:14 +0200

    Update page.tsx

--- app/(tabs)/livres/[id]/page.tsx
+++ app/(tabs)/livres/[id]/page.tsx
@@ -643,7 +643,6 @@ const saveTitle = () => {
                     <input
                       type="file"
                       accept="image/*"
-                      capture="environment"
                       onChange={(e) => {
                         const file = e.target.files?.[0];
                         if (file) handleCoverImageUpload(file);
