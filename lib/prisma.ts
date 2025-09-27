c7c7f0212b7bbcaa0502c87e39950413500b4833
OLIVIER TANGOPOULOS <oliviertangopoulos@MacBook-Pro-de-OLIVIER.local>
2025-09-27 09:30:22 +0200

    maj prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,23 +1,24 @@
 // lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
-import { neon } from '@neondatabase/serverless'
+import { neon, neonConfig } from '@neondatabase/serverless'
 import { PrismaNeon } from '@prisma/adapter-neon'
 
 declare global {
-  // Trick to avoid multiple Prisma instances in Next.js (Hot Reload in dev)
-  // @ts-ignore
+  // Hot reload fix for Next.js
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined
 }
+
+// Required for Prisma + Neon
+neonConfig.poolQueryViaSocket = true
 
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
-  // In production (Vercel), use Neon serverless client
   const connection = neon(process.env.DATABASE_URL!)
   const adapter = new PrismaNeon(connection)
   prisma = new PrismaClient({ adapter })
 } else {
-  // In dev, reuse the same client to avoid exhausting Neon connections
   if (!global.prisma) {
     const connection = neon(process.env.DATABASE_URL!)
     const adapter = new PrismaNeon(connection)
