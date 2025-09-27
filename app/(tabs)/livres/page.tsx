d6e36d9a71034d5276f2a1c7e27a71b8e3203d5b
OLIVIER TANGOPOULOS <oliviertangopoulos@MacBook-Pro-de-OLIVIER.local>
2025-09-27 10:36:56 +0200

    maj livre

--- app/(tabs)/livres/page.tsx
+++ app/(tabs)/livres/page.tsx
@@ -187,7 +187,7 @@ function BooksLoadingSkeleton() {
                     <div className="flex gap-2">
                       <Link
                         href={`/livres/${book.id}`}
-                        className="inline-flex-1 bg-green-500 text-white py-2 px-4 md:py-2 rounded-lg hover:bg-bleu-400 transition-colors font-medium text-xs md:text-sm text-center"
+                        className="inline-flex-1 bg-green-700 text-white py-2 px-4 md:py-2 rounded-lg hover:bg-green-700 transition-colors font-medium text-xs md:text-sm text-center"
                       >
                         Voir le livre
                       </Link>
